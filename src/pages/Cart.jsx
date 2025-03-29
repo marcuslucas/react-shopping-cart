2c2c9d48898ece94036a7bb73ea3ae31afd280dc
Marcus Lucas <marcuslucas5680@gmail.com>
2025-03-29 14:30:53 -0400

    Added search funtionality

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CartContext } from "../context/CartContext";
 import { useContext } from "react";
-import ProductCard from "../components/ProductCard";
+
 import { Link } from "react-router-dom";
 
 const Cart = () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { Link } from "react-router-dom";
 import useFetchedProducts from "../hooks/useFetchProducts";
 
 const Home = () => {
   const { products, loading, error } = useFetchedProducts();
+  const [searchQuery, setSearchQuery] = useState("");
 
   if (loading) {
     return <h2>Loading products</h2>;
@@ -14,13 +15,35 @@ const Home = () => {
     return <h2>{error}</h2>;
   }
 
+  // const filteredProducts = products.filter((product) =>
+  //   product.title.toLowerCase().includes(searchQuery.toLowerCase())
+  // );
+
+  const filteredProducts =
+    searchQuery.trim() === ""
+      ? products
+      : products.filter((product) =>
+          product.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
   return (
     <div>
       <h1>This is the home page</h1>
+      <input
+        type="text"
+        placeholder="Search for an item"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
       <Link to="cart">View Cart</Link>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      {filteredProducts.length > 0 ? (
+        filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      ) : (
+        <p>No Products Found</p>
+      )}
     </div>
   );
 };
